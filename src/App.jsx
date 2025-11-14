9a0946b4d2720f186e3b3b06597f2774fbae86b7
ZHAO JIAYI <z261459808@gmail.com>
2025-11-14 22:33:33 +0900

    next

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import NavBar from "./components/NavBar";
+import ShopList from "./components/shopList";
 export default function App() {
   return (
     <>
       <NavBar />
+      <ShopList />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-
+import "primeflex/primeflex.css";
+import "primereact/resources/primereact.css";
 import { PrimeReactProvider } from "primereact/api";
 
 import "primeicons/primeicons.css";
